843885fad84628159262a9150472ce8f12244822
iceland <75991805+iceland2k14@users.noreply.github.com>
2021-07-12 15:23:26 +0530

    File Size Display

--- v6_dll_bsgs/bsgs_create_bpfile_bloomfile.py
+++ v6_dll_bsgs/bsgs_create_bpfile_bloomfile.py
@@ -166,8 +166,16 @@ if __name__ == '__main__':
         total = num_cpu*1000*(total//(num_cpu*1000))
         print('[*] Number of elements should be a multiple of 1000*num_cpu. Automatically corrected it to nearest value:',total)
     w = math.ceil(math.sqrt(total))
+    
+    bloom_prob = 0.000000001                # False Positive = 1 out of 1 billion
+    bloom_bpe = -(math.log(bloom_prob) / 0.4804530139182014)
+    
+    bloom_bits = int(total * bloom_bpe)  # ln(2)**2
+    if bloom_bits % 8: bloom_bits = 8*(1 + (bloom_bits//8))
+    bloom_hashes = math.ceil(0.693147180559945 * bloom_bpe)
+    
     print('[+] Number of items required for Final Script : [bp : {0}] [bloom : {1}]'.format(w, total))
-    print('[+] Output Size of the files : [bp : {0} Bytes] [bloom : {1} Bytes]'.format(w*32, total*5))
+    print('[+] Output Size of the files : [bp : {0} Bytes] [bloom : {1} Bytes]'.format(w*32, bloom_bits//8))
     print('[+] Creating bpfile in range {0} to {1} '.format(1, w))
     
     
@@ -181,13 +189,7 @@ if __name__ == '__main__':
     
 ###############################################################################
     
-    bloom_prob = 0.000000001                # False Positive = 1 out of 1 billion
-    bloom_bpe = -(math.log(bloom_prob) / 0.4804530139182014)
-    
-    bloom_bits = int(total * bloom_bpe)  # ln(2)**2
-    if bloom_bits % 8: bloom_bits = 8*(1 + (bloom_bits//8))
-    bloom_hashes = math.ceil(0.693147180559945 * bloom_bpe)
-    
+
     print('[+] Starting bloom file creation ... with False Positive probability :', bloom_prob)
     print('[+] bloom bits  :', bloom_bits, '   size [%s MB]'%(bloom_bits//(8*1024*1024)))
     print('[+] bloom hashes:', bloom_hashes)
